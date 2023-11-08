bff3bd2bd7ac540eb26f0e49bb4089e3e0647b36
Don Do <donnios@Dons-MacBook-Air.local>
2023-11-08 09:45:04 -0800

    update loading

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -180,7 +180,7 @@ export default function Games() {
 
       return (
         <>
-        {(bballScore === null || (Array.isArray(bballScore) && bballScore.length === 0)) && (<h1 className='loading'>No NBA Games Today</h1>)}            
+        {/* {(bballScore === null || (Array.isArray(bballScore) && bballScore.length === 0)) && (<h1 className='loading'>No NBA Games Today</h1>)}             */}
         {isPending && <h1 className = "loading">Loading Games...</h1>}
         {invalidBet && <h1 className = "betFail loading">Bet Unsuccessful...</h1>}
         {validBet && <h1 className = "betSuccess loading">Bet Successful!</h1>}
